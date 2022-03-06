94c22ca83fd7971676a56e1969c4ac86c2b11d4b
samira talanchi <samira73talanchi@gmail.com>
2022-03-06 20:23:28 +0330

    add alphabet keyboard function

---
+++ myjs.js
@@ -0,0 +1,114 @@
+var i=1
+var j=1;
+function addQ() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="Q";
+    j++;
+}
+function addW() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="W";
+    j++;
+}
+function addE() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="E";
+    j++;
+}
+function addR() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="R";
+    j++;
+}
+function addT() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="T";
+    j++;
+}
+function addU() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="U";
+    j++;
+}
+function addY() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="Y";
+    j++;
+}
+function addI() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="I";
+    j++;
+}
+function addO() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="O";
+    j++;
+}
+function addP() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="P";
+    j++;
+}
+function addU() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="U";
+    j++;
+}
+function addA() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="A";
+    j++;
+}
+function addS() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="S";
+    j++;
+}
+function addD() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="D";
+    j++;
+}
+function addF() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="F";
+    j++;
+}
+function addT() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="T";
+    j++;
+}
+function addG() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="G";
+    j++;
+}
+function addH() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="H";
+    j++;
+}
+function addJ() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="J";
+    j++;
+}
+function addK() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="K";
+    j++;
+}
+function addL() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="L";
+    j++;
+}
+function addZ() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="Z";
+    j++;
+}
+function addX() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="X";
+    j++;
+}
+function addC() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="C";
+    j++;
+}
+function addV() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="V";
+    j++;
+}
+function addB() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="B";
+    j++;
+}
+function addN() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="N";
+    j++;
+}
+function addM() {
+    document.getElementById("b"+i+"letter"+j).innerHTML="M";
+    j++;
+}
